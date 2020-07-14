d34ad30d4efcfd37e0279feb3d9f8526e4beb9ee
DKFactotum <daniil.koshelyuk@gmail.com>
2020-07-14 14:20:11 +0200

    fix for new ARCore version

---
+++ Scripts/Extension/DetectedPlaneVisualizerExtender.cs
@@ -0,0 +1,25 @@
+#if UNITY_ANDROID
+using UnityEngine;
+using GoogleARCore.Examples.Common;
+using GoogleARCore;
+
+public class DetectedPlaneVisualizerExtender : MonoBehaviour {
+	public static bool flagVisible = true;
+	// private string layerScanMesh = "Spatial Awareness";
+	private MeshRenderer m_MeshRenderer;
+	private MeshCollider m_MeshCollider;
+    
+	public void Awake()
+	{
+		m_MeshRenderer = GetComponent<MeshRenderer>();
+		// gameObject.AddComponent<MeshCollider>();
+		// gameObject.layer = LayerMask.NameToLayer(this.layerScanMesh);
+		m_MeshCollider = GetComponent<MeshCollider>();
+	}
+    
+	public void Update(){
+		m_MeshCollider.sharedMesh = GetComponent<MeshFilter>().mesh;
+		m_MeshRenderer.enabled = DetectedPlaneVisualizerExtender.flagVisible;
+	}
+}
+#endif
